0f3df59ff0f9c3833722145a3aaf35dc04f4e894
Samrudhhi Gaikwad <gaikwadsamruddhi820@gmail.com>
2026-02-05 15:12:09 +0530

    made a folder for 3rd assignment

--- Assignment 3/pr2.js
+++ Assignment 3/pr2.js
@@ -17,7 +17,7 @@ console.log(arr.join(""));
 
 console.log("---------------------------------------")
 
-//pr6 . Filter number greater than 5 answer = [9, 10, 7] .User array.filter
+//pr6 . gFilter number greater than 5 answer = [9, 10, 7] .User array.filter
 let inputArr = [1,2,3,9,10,7,5,4,3]
 
 let output=inputArr.filter(num => num>5);
