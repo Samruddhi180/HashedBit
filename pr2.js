ea879917d93bec39d29b42032114ee0f32968c1c
Samrudhhi Gaikwad <gaikwadsamruddhi820@gmail.com>
2026-02-05 15:04:52 +0530

    Modified js files

--- pr2.js
+++ pr2.js
@@ -4,3 +4,23 @@ let str='I love my India';
 
 let res=str.split(' ').reverse().join(' ');
 console.log(res);
+
+//Q3) let string = 'INDIA' output = 'INDONESIA' . Use array.splice
+console.log("---------------------------------------")
+let str1='INDIA';
+
+let arr=str1.split("");
+arr.splice(3,0,'O','N','E','S');
+
+console.log(arr.join(""));
+
+
+console.log("---------------------------------------")
+
+//pr6 . Filter number greater than 5 answer = [9, 10, 7] .User array.filter
+let inputArr = [1,2,3,9,10,7,5,4,3]
+
+let output=inputArr.filter(num => num>5);
+console.log(output);
+
+
--- pr6.js
+++
@@ -1,5 +0,0 @@
-//Filter number greater than 5 answer = [9, 10, 7] .User array.filter
-let inputArr = [1,2,3,9,10,7,5,4,3]
-
-let output=inputArr.filter(num => num>5);
-console.log(output);
